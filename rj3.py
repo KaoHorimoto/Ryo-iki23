048bd5e7a2264ebc8cdf3060fe5ef8e1895968f5
KaoHorimoto <hqv85385@kwansei.ac.jp>
2023-06-21 16:08:17 +0900

    課題3 奇数の合計

--- rj3.py
+++ rj3.py
@@ -2,7 +2,8 @@ sum = 0
 
 for i in range(0, 1000):
     file_name = f'kitamura_{i:05d}_kgu.txt'
-    with open(file_name, 'r') as file:
+    path = f'sample/{file_name}'
+    with open(path, 'r') as file:
         number = int(file.read())
         if number % 2 != 0:
             sum += number
