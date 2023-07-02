d5345469064e9a60a9cdc56155f4389297700c7e
KaoHorimoto <hqv85385@kwansei.ac.jp>
2023-07-02 20:55:58 +0900

    図形の移動

---
+++ rj5.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+def animation():
+    canvas.move('all', 5, 0)
+    canvas.after(100, animation)
+
+root = tk.Tk()
+root.geometry('700x700')
+
+canvas = tk.Canvas(root, width=700, height=700)
+canvas.pack()
+
+head = canvas.create_oval(300, 100, 400, 200,  width=3)
+body = canvas.create_line(350, 200, 350, 300, fill='black', width=3)
+left_leg = canvas.create_line(350, 300, 280, 400, fill='black', width=3)
+right_leg = canvas.create_line(350, 300, 420, 400, fill='black', width=3)
+left_arm = canvas.create_line(350, 240, 280, 200, fill='black', width=3)
+right_arm = canvas.create_line(350, 240, 420, 200, fill='black', width=3)
+
+animation()
+
+root.mainloop()
