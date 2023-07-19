d152cc3bd88f8b81cc039268c60238386e62949f
KaoHorimoto <hqv85385@kwansei.ac.jp>
2023-07-19 15:35:18 +0900

    課題6-2

--- rj6_2.py
+++ rj6_2.py
@@ -15,9 +15,9 @@ person1 = people_data[0]
 person2 = people_data[1]
 
 root = tk.Tk()
-root.geometry("1000x800")
+root.geometry("1300x800")
 
-canvas = tk.Canvas(root, width=1000, height=800)
+canvas = tk.Canvas(root, width=1300, height=800)
 canvas.pack()
 
 right_shoulder_x = person1["pose_keypoints_2d"][6]
@@ -27,8 +27,8 @@ left_shoulder_y = person1["pose_keypoints_2d"][16]
 neck_x = person1["pose_keypoints_2d"][3]
 neck_y = person1["pose_keypoints_2d"][4]
 
-canvas.create_line(neck_x-900, neck_y, right_shoulder_x-900, right_shoulder_y, fill = "red", width=2)
-canvas.create_line(neck_x-900, neck_y, left_shoulder_x-900, left_shoulder_y, fill = "red", width=2)
+canvas.create_line(neck_x, neck_y, right_shoulder_x, right_shoulder_y, fill = "red", width=2)
+canvas.create_line(neck_x, neck_y, left_shoulder_x, left_shoulder_y, fill = "red", width=2)
 
 right_shoulder_x = person2["pose_keypoints_2d"][6]
 right_shoulder_y = person2["pose_keypoints_2d"][7]
